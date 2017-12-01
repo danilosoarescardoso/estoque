d4a0c2cbc08aa78466cac4ca0fe3dea7ed8b4fa0
Danilo Soares <dsc.soares@gmail.com>
2017-12-01 17:46:35 +0000

    Refatoração de views

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -7,9 +7,11 @@ class ProdutoController extends Controller {
 
     public function lista(){
 
+        
+
         $produtos = DB::select('select * from produtos');
 
-        return view('listagem')->with('produtos', $produtos);
+        return view('produto.listagem')->with('produtos', $produtos);
 
     }
 
@@ -20,6 +22,6 @@ class ProdutoController extends Controller {
         if(empty($resposta)) {
             return "Esse produto não existe";
         }
-        return view('detalhes')->with('p', $resposta[0]);
+        return view('produto.detalhes')->with('p', $resposta[0]);
     }
 }
